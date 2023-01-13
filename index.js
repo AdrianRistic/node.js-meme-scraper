1bd51329a77e5d3ec3615991a185e3a44e5f19d8
Adrian N. Ristic <toffees.taco.0f@icloud.com>
2023-01-13 11:49:51 +0100

    MADE it more beautiful

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ import path from 'path';
 const memeURL = 'https://memegen-link-examples-upleveled.netlify.app';
 
 const response = await fetch(memeURL);
-const body = await response.text();
+const htmlContent = await response.text();
 
-//const htmlContent = console.log(body);
-
-let $ = load(body);
+let $ = load(htmlContent);
 
 {
-  $ = load(body);
+  $ = load(htmlContent);
   const path = $('img').attr('src');
   console.log(path);
 }
