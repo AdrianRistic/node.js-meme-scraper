0944c204082d61314a2503df4f32b84f7fca28c2
Adrian N. Ristic <toffees.taco.0f@icloud.com>
2023-01-15 21:46:48 +0100

    Fixed Links + rudi img

--- index.js
+++ index.js
@@ -51,11 +51,65 @@ async function getMemes() {
     for (let i = 3; i < 13; i++) {
       console.log(items[i]);
     }
-    //console.log(items[3]);
+    console.log(items[3]);
+    return items;
   } catch (error) {
     console.log(error);
   }
 }
 
+async function memeprinter(items) {
+  const payload = new ImagesPayload({
+    // example headers
+    headers: {
+      Cookie: 'name=cookie',
+    },
+
+    // http or https agent
+  });
+  const {
+    // Image Data
+    image,
+    // Image size in bytes
+    size,
+    // MIME ContentType
+    contentType,
+    // File extension - png, jpg and etc.
+    type,
+    // Image Url
+    url,
+    // Download status
+    downloaded,
+    //  Image hash
+    hash,
+  } = await payload.request(
+    'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+  );
+  fs.writeFileSync('../memes/spicymeme.png', image.source);
+
+  items[3] =
+    'https://memecomplete.com/edit/https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg';
+  for (let i = 3; i < 13; i++) {
+    const {
+      image,
+      // Image size in bytes
+      size,
+      // MIME ContentType
+      contentType,
+      // File extension - png, jpg and etc.
+      type,
+      // Image Url
+      url,
+      // Download status
+      downloaded,
+      //  Image hash
+      hash,
+    } = await payload.request(items[i]);
+    const custom = '../memes/spicymeme' + i + '.jpg';
+    fs.writeFileSync(custom, image.source);
+  }
+}
+
 // Run getMemes
-getMemes();
+const items = getMemes();
+memeprinter(items);
